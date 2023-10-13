edf0d3b335db4bd6ad688b2fff5200a1e2a06e97
@The Jack <abhishekdumaniya196@gmail.com>
2023-10-13 23:19:54 +0530

    Files add Passenger counter app

---
+++ Passenger counter app/index.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
