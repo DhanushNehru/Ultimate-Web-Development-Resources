c0ab79e3d351e700e0e39b7d01177c5fc6cb9b8d
Divyanshi Narang <74779601+DivyanshiNarang@users.noreply.github.com>
2023-10-17 23:35:49 +0530

    Update script.js

--- Temperature_convertor/script.js
+++ Temperature_convertor/script.js
@@ -7,18 +7,18 @@ function computeTemp(event) {
 
   switch (event.target.name) {
     case "celsius":
-      kelvinEl.value = (currentValue + 273.32).toFixed(2);
+      kelvinEl.value = (currentValue + 273.15).toFixed(2);
       fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
       break;
 
     case "fahrenheit":
       celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
-      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.15).toFixed(2);
       break;
 
     case "kelvin":
-      celsiusEl.value = (currentValue - 273.32).toFixed(2);
-      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      celsiusEl.value = (currentValue - 273.15).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.15) * 1.8 + 32).toFixed(2);
       break;
     default:
       break;
